96df57d9eff5d73491b51dbd51a4fa8ace6a73d9
Akash9634 <akashkush2020@gmail.com>
2025-11-06 02:58:31 +0530

    connections page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Profile from "./Components/Profile";
 import { Provider } from "react-redux";
 import Feed from "./Components/Feed";
 import appStore from "./utils/appStore";
+import Connections from "./Components/Connections";
+import Requests from "./Components/Requests";
 
 function App() {
 
@@ -17,6 +19,8 @@ function App() {
         <Route path="/feed" element={<Feed/>}/>
        <Route path="/login" element={<Login/>}/>
        <Route path="/profile" element={<Profile/>}/>
+       <Route path="/connections" element={<Connections/>}/>
+       <Route path="/requests" element={<Requests/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
